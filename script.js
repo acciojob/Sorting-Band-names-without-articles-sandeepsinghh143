a8a78758437d7168906199c240a600011e9f1c1f
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-19 14:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
+function sortBandNames(bandNames) {
+  let modifiedBandNames = [];
 
+  for (let i = 0; i < bandNames.length; i++) {
+    let modifiedName = bandNames[i].replace(/^(a|an|the)\s+/i, '');
+    modifiedBandNames.push(modifiedName);
+  }
+
+  modifiedBandNames.sort();
+
+  let ulElement = document.getElementById('band');
+  ulElement.innerHTML = '';
+
+  for (let i = 0; i < modifiedBandNames.length; i++) {
+    let liElement = document.createElement('li');
+    liElement.textContent = modifiedBandNames[i];
+    ulElement.appendChild(liElement);
+  }
+}
